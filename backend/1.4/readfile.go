ed1b7ca914117417e8177e936bcb5e0946c3e5a3
Ilya Evgrafov <iaevgrafov@avito.ru>
2025-09-29 17:40:27 +0300

    Создал директорию под модуль 1.4. Практиковался с os

---
+++ backend/1.4/readfile.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	err := os.WriteFile("hellofile.txt", []byte("Привет программист!"), 0600)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, err := os.ReadFile("hellofile.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(data))
+	err = os.Remove("hellofile.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
