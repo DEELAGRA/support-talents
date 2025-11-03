ebd000abd8dc2aef8c29aa8b836a8a7656e7d085
DEELAGRA <deelagra@gmail.com>
2025-11-03 13:21:51 +0300

    Исправил http_json

--- backend/1.5/http_json/main.go
+++ backend/1.5/http_json/main.go
@@ -1,21 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
-type MyHandler struct {
-	ID   int
-	Name string
-}
+type MyHandler struct{}
 
 func (h MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Привет ты запустил сервер твой ID: %v\n", h.ID)
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Только метод POST", http.StatusMethodNotAllowed)
+		return
+	}
+	var text map[string]interface{}
+	if err := json.NewDecoder(r.Body).Decode(&text); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	if text == nil {
+		http.Error(w, "Отправь тело json", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(text)
+
 }
 
 func main() {
 
-	fmt.Println("Сервер зарущен на http://localhost:8080")
-	http.ListenAndServe(":8080", MyHandler{})
+	fmt.Println("Сервер запущен на http://localhost:8080")
+	http.ListenAndServe(":8080", &MyHandler{})
 }
